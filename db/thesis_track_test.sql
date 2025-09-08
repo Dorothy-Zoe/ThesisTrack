3eed329b050c534f212131e62de7983e99c6f0e5
Dorothy Balmoja <106675870+Dorothy-Zoe@users.noreply.github.com>
2025-09-08 21:09:44 +0800

    Update test database to use thesis_track_test.sql for safe unit testing
    Modified the unit testing setup to use a separate test database defined in thesis_track_test.sql instead of the main thesis_track.sql.

--- db/thesis_track_test.sql
+++ db/thesis_track_test.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 18, 2025 at 10:18 AM
+-- Generation Time: Sep 08, 2025 at 03:07 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `thesis_track`
+-- Database: `thesis_track_test`
 --
 
 -- --------------------------------------------------------
